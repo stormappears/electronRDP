403288bc711ecf876770555ea677a22158868892
THIS IS NOT ME <92795413+stormappears@users.noreply.github.com>
2023-12-25 12:38:01 +0530

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,14 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer, ipcMain } = require("electron");
+const os = require("os");
+
+contextBridge.exposeInMainWorld("electron", {
+  arch: () => os.arch(),
+
+  getScreenId: (callback) => ipcRenderer.on("SET_SOURCE_ID", callback),
+
+  setSize: (size) => ipcRenderer.on("set-size", size),
+
+  getUserId: (id) => ipcRenderer.send("get-id", id),
+
+  getMinimiseAction: (id) => ipcRenderer.send("get-minimise", id),
+});
